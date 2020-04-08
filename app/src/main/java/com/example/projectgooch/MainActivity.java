4bfd79979b574ebd45ae61eee615becae51d3e61
zjohnso <squaids.9225@gmail.com>
2020-04-08 03:03:52 -0400

    Added assets, forced immersive mode, tested armor overlay

--- app/src/main/java/com/example/projectgooch/MainActivity.java
+++ app/src/main/java/com/example/projectgooch/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.projectgooch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
