58ebc5c6d4a22c4cff6df1f06b04cacaf2f5daa9
zjohnso <squaids.9225@gmail.com>
2020-04-09 02:57:23 -0400

    Added buttons

--- app/src/main/java/com/example/projectgooch/MainActivity.java
+++ app/src/main/java/com/example/projectgooch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.projectgooch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -12,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displaySkillButtons();
+    }
+
+    private void displaySkillButtons() {
+        loadFragment(new SkillsSelectFragment());
+    }
+
+    private boolean loadFragment(Fragment f) {
+        if (f != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, f).commit();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/example/projectgooch/SkillsSelectFragment.java
@@ -0,0 +1,20 @@
+package com.example.projectgooch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SkillsSelectFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_skill_select, container, false);
+        return v;
+    }
+}
